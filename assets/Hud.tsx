903e374b3c869e979a2dc557cf9502990ffe7df7
lawlaw <tech@l-z.fr>
2026-04-07 20:27:41 +0200

    Add HUD assets and update maps to use HUD tiles

---
+++ assets/Hud.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Hud" tilewidth="16" tileheight="32" tilecount="5" columns="5">
+ <image source="hud.png" width="80" height="32"/>
+</tileset>
