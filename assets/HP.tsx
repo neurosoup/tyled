0dc91493377b029c18606e493bc23beaea071e7d
law <tech@l-z.fr>
2026-05-08 17:36:44 +0200

    Cleanup: remove unused asset files and update atlas/hud assets

--- assets/HP.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="hp" tilewidth="16" tileheight="32" tilecount="2" columns="2">
- <image source="hp.png" width="32" height="32"/>
-</tileset>
--- assets/atlas1.tsx
+++ assets/atlas1.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Atlas1" tilewidth="16" tileheight="16" tilecount="512" columns="32">
- <image source="atlas1.png" width="512" height="256"/>
+<tileset version="1.10" tiledversion="1.12.1" name="Atlas1" tilewidth="16" tileheight="16" tilecount="640" columns="32">
+ <image source="atlas1.png" width="512" height="320"/>
  <tile id="64">
   <properties>
    <property name="Ground" type="class" propertytype="tyled::components::markers::Ground"/>
