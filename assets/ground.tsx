4811af56e820a08d5c2cd9c6fef8fc39ec57fa76
law <tech@l-z.fr>
2026-05-31 12:50:55 +0200

    Add new assets

---
+++ assets/ground.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="ground" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="ground.png" width="192" height="192"/>
+ <tile id="15">
+  <properties>
+   <property name="ForbiddenArea" type="class" propertytype="tyled::components::markers::ForbiddenArea"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Ground" type="class" propertytype="tyled::components::markers::Ground"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/players.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="players" tilewidth="16" tileheight="16" tilecount="144" columns="12" fillmode="preserve-aspect-fit">
+ <tileoffset x="1" y="-4"/>
+ <image source="players.png" width="192" height="192"/>
+</tileset>
